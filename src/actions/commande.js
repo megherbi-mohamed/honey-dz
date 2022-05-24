518fb48e6e606ce8812265db1a1a7ce51b36ff48
megherbi mohamed <megherbimeddz@gmail.com>
2022-05-24 20:38:03 +0100

    aa

--- src/actions/commande.js
+++ src/actions/commande.js
@@ -8,7 +8,7 @@ export const insertCommande = async (formData, navigate) => {
         const {data} = await api.insertCommande(formData);
         console.log(data);
         // dispatch({ type: AUTH, data });
-        // navigate('/account');
+        navigate('/account');
     } catch (error) {
         console.log(error);
     }
