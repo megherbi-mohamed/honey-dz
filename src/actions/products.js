603f1cd653048c295bfc45980ae0f1ee0c1d01d3
megherbi mohamed <megherbimeddz@gmail.com>
2022-06-18 15:09:09 +0100

    a

--- src/actions/products.js
+++ src/actions/products.js
@@ -22,8 +22,6 @@ export const getProduct = (id) => async (dispatch) => {
   try {
     const { data } = await api.getProduct(id);
     const product = [{id:data._id,front:data.front,side:data.side,nom:data.nom,price:data.prix,color:data.color,size:''}];
-    // const result = Object.keys(product).map((key) => [Number(key), product[key]]);
-    // console.log(result);
     dispatch({ type: "GET_PRODUCT", payload: product });
   } catch (error) {
     console.log(error.message);
