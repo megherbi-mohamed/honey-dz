426068bcf132759b61f2611cfb4f446460bb5a72
megherbi mohamed <megherbimeddz@gmail.com>
2022-06-20 12:02:23 +0100

    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
-import React,{useState,useEffect} from 'react'
+import React,{useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getAllProducts } from '../actions/products';
-import * as api from "../api";
 
 import Product from './Product'
 
@@ -19,22 +18,15 @@ const Home = () => {
 
     // let initProducts = [{},{},{},{},{},{},{},{},{}];
 
-    const dispatch = useDispatch();
+    const {products} = useSelector((state) => state.products);
 
-    const [products, setproducts] = useState([])
+    const dispatch = useDispatch();
 
     useEffect(() => {
         // if (products.length === 0) {
             // dispatch(getAllProducts());
         // }
-        (async () => {
-            const { data } = await api.getAllProducts();
-            setproducts(data)
-        })();
-        
     },[])
-
-    // const {products} = useSelector((state) => state.products);
     
     // if (products.length === 0) {
     //     return (
