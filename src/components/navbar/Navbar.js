b0e46889ded601726ac4109f23f24a925f0ce97b
megherbi mohamed <megherbimeddz@gmail.com>
2022-06-22 14:33:15 +0100

    a

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -86,8 +86,8 @@ const Navbar = () => {
                         <Link to={user ? '/account' : '/account/signin'} className="hidden lg:block py-[14px] px-[13px]">
                             <FontAwesomeIcon icon={faUser} className="text-[13px] text-[#bd8c27] w-[20px] h-[13px]" />
                         </Link>
-                        <div className="relative flex items-end py-[8px] pl-[13px] pr-[6px] md:px-[13px]">
-                            <svg className="w-[13px] h-[20px] cursor-pointer" fill="#bd8c27" onClick={()=>displayCart()} stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 412"><path d="M352 128C352 57.42 294.579 0 224 0 153.42 0 96 57.42 96 128H0v304c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V128h-96zM224 48c44.112 0 80 35.888 80 80H144c0-44.112 35.888-80 80-80zm176 384c0 17.645-14.355 32-32 32H80c-17.645 0-32-14.355-32-32V176h48v40c0 13.255 10.745 24 24 24s24-10.745 24-24v-40h160v40c0 13.255 10.745 24 24 24s24-10.745 24-24v-40h48v256z"></path></svg>
+                        <div className="relative flex items-end py-[8px] pl-[13px] pr-[6px] md:px-[13px]" onClick={()=>displayCart()} >
+                            <svg className="w-[13px] h-[20px] cursor-pointer" fill="#bd8c27" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 412"><path d="M352 128C352 57.42 294.579 0 224 0 153.42 0 96 57.42 96 128H0v304c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V128h-96zM224 48c44.112 0 80 35.888 80 80H144c0-44.112 35.888-80 80-80zm176 384c0 17.645-14.355 32-32 32H80c-17.645 0-32-14.355-32-32V176h48v40c0 13.255 10.745 24 24 24s24-10.745 24-24v-40h160v40c0 13.255 10.745 24 24 24s24-10.745 24-24v-40h48v256z"></path></svg>
                             {isEmpty ? <div></div> : (
                                 <div className="w-[22px] h-[22px] rounded-full bg-[#da3f3f] relative">
                                     <span className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white text-[12px]">{totalUniqueItems}</span>
