1952678fe8f3e67e1dcb3a7dce36ce34e2f29464
megherbi mohamed <megherbimeddz@gmail.com>
2022-06-22 12:27:17 +0100

    a

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
     return (
         <div className="w-full z-0 mt-[56px] md:mt-[64px]">
-            {/* <Slider/> */}
+            <Slider/>
             <div className="w-full overflow-x-auto no-scrollbar md:overflow-hidden">
                 <div className="w-full lg:max-w-[calc(1280px+calc(35px/1.25)*2)] border-box mx-auto mb-[0] mt-[50px] md:mt-[60px] md:my-[60px] lg:mt-[80px] lg:mx-auto px-[6px] lg:px-[calc(35px/1.25)] flex flex-row md:grid md:gap-0 lg:gap-[30px] md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-start">
                     <Suspense fallback={
